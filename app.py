15efe424fa6dcf6f9b3c3130563577632115f75c
Armellectx <56161210+Armellectx@users.noreply.github.com>
2023-12-12 10:10:24 +0100

    Update app.py
    Clean

--- app.py
+++ app.py
@@ -39,15 +39,12 @@ def display_result(detailed_predictions, c2, name_image):
     
     n_boar = 0
     n_deer = 0
-    print(detailed_predictions)
     for animal in detailed_predictions:
         ecrire_dans_csv(name_image, animal[1], animal[0], "prédictions.csv")
         if animal[1]=="Wild_Boar":
             n_boar += 1
         else:
             n_deer += 1
-    print("LA DETECTION")
-    print(n_boar," ", n_deer)
     if n_boar*n_deer!=0:
         c2.write(f"Au total {len(detailed_predictions)} animaux sauvages détectés sur cette image.\n\n")
         c2.write(f"Nombre de sanglier(s): {n_boar}")
@@ -62,56 +59,12 @@ def display_result(detailed_predictions, c2, name_image):
     else:
         c2.write("Pas de cerfs ni de sangliers détectés sur cette image")
         
-    
-
-def on_upload_bug(file, model_weight_path, conf):
-    image = Image.open(BytesIO(file))
-    image = image.resize((400, 400))
-    image = np.asarray(image)
-    image = np.expand_dims(image, axis=0)
-    #image = cv2.imread(file)
-    model = YOLO(model_weight_path)
-    #print("LE TYPE DE LIM")
-    #print(type(image))
-    #print(image.shape)
-    #image = cv2.resize(image, (640, 640))
-    results = model(image)[0]
-    initial_image = results.orig_img
-
-    detailed_predictions = []
-    for box in results.boxes.data.tolist():
-        x1, y1, x2, y2, score, class_id = box
-        print("score: ", score)
-        print("classe_id: ", class_id)
-        class_id = int(class_id)
-        if class_id == 0:
-            class_label = "Deer"
-            class_color = (80, 200, 225)
-        else:
-            class_label = "Wild_Boar"
-            class_color = (240, 100, 250)
-        
-        if score>conf:
-            image_res = cv2.rectangle(initial_image,(int(x1), int(y1)), (int(x2), int(y2)), class_color, 3)
-            image_res = cv2.putText(image_res, class_label + " "+str(int(score*100)) + "%", (int(x1), int(y1 - 10)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, class_color, 3, cv2.LINE_AA)
-        
-        detailed_predictions.append((score, class_label))
-
-
-        return image_res, detailed_predictions
-
+#fonction de traitement d'une image fournie
 def on_upload(results, conf):
-
     initial_image = results.orig_img
-
     detailed_predictions = []
-    print(results.boxes.data.tolist())
-    print(len(results.boxes.data.tolist()))
     for box in results.boxes.data.tolist():
         x1, y1, x2, y2, score, class_id = box
-        print("score: ", score)
-        print("classe_id: ", class_id)
         class_id = int(class_id)
         if class_id == 0:
             class_label = "Deer"
@@ -119,7 +72,6 @@ def on_upload(results, conf):
         else:
             class_label = "Wild_Boar"
             class_color = (240, 100, 250)
-        print(class_label)
         if score>conf:
             detailed_predictions.append((score, class_label))
             image_res = cv2.rectangle(initial_image,(int(x1), int(y1)), (int(x2), int(y2)), class_color, 2)
@@ -127,48 +79,9 @@ def on_upload(results, conf):
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, class_color, 1, cv2.LINE_AA)
         else:
             image_res = initial_image
-        
-
-
+    
     return image_res, detailed_predictions 
 
-
-def on_upload_bug(file, model_weight_path, conf):
-    image = Image.open(BytesIO(file))
-    image = image.resize((400, 400))
-    image = np.asarray(image)
-    image = np.expand_dims(image, axis=0)
-    #image = cv2.imread(file)
-    model = YOLO(model_weight_path)
-    #print("LE TYPE DE LIM")
-    #print(type(image))
-    #print(image.shape)
-    #image = cv2.resize(image, (640, 640))
-    results = model(image)[0]
-    initial_image = results.orig_img
-
-    detailed_predictions = []
-    for box in results.boxes.data.tolist():
-        x1, y1, x2, y2, score, class_id = box
-        print("score: ", score)
-        print("classe_id: ", class_id)
-        class_id = int(class_id)
-        if class_id == 0:
-            class_label = "Deer"
-            class_color = (80, 200, 225)
-        else:
-            class_label = "Wild_Boar"
-            class_color = (240, 100, 250)
-        
-        if score>conf:
-            image_res = cv2.rectangle(initial_image,(int(x1), int(y1)), (int(x2), int(y2)), class_color, 3)
-            image_res = cv2.putText(image_res, class_label + " "+str(int(score*100)) + "%", (int(x1), int(y1 - 10)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, class_color, 3, cv2.LINE_AA)
-        
-        detailed_predictions.append((score, class_label))
-
-
-        return image_res, detailed_predictions  
 def app_basic_display():
     st.markdown(
     """
@@ -188,22 +101,22 @@ def main():
 
     st.write("\n\n")
     c1, c2 = st.columns(2)
+    
+    #indice de confiance à partir duquel les images sont labélisées
     conf = 0.6
     if upload:
         name_image = "images/"+upload.name
         image = cv2.imread(name_image)
-        #model = YOLO("yolov8n.pt")
+        #model = YOLO("yolov8n.pt") #model de base préentrainé
         path_w = 'weights'
         model = YOLO(path_w + "/best.pt")
         results = model.predict(image)
-        
         
         result_image, predictions = on_upload(results[0], conf)
         c1.image(result_image)
         display_result(predictions, c2, name_image)
         #c1.image((Image.open(result_image)))
   
-
 
 if __name__ == "__main__":
     main()
